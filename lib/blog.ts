72e1fb66c3b8e06b629beec1472b5fd1516dd086
maxklema <maxklema@gmail.com>
2026-01-11 14:27:36 -0500

    blog page writings page and some lib functions

--- lib/blog.ts
+++ lib/blog.ts
@@ -6,6 +6,7 @@ interface BlogMetadata {
   title: string;
   date: string;
   description: string;
+  slug: number;
 }
 
 interface BlogPost {
@@ -13,7 +14,7 @@ interface BlogPost {
   content: string;
 }
 
-const blogPath = path.join(process.cwd(), '..', 'content/blogs');
+const blogPath = path.join(process.cwd(), 'content/blogs');
 
 export async function getAllBlogs(): Promise<BlogMetadata[]> {
     const res: BlogMetadata[] = [];
@@ -22,17 +23,24 @@ export async function getAllBlogs(): Promise<BlogMetadata[]> {
         const files = await fs.promises.readdir( blogPath );
 
         for (const file of files){
+            if (!file.endsWith('.md')) continue;
+            
             const slugPath = path.join(blogPath, file);
             const blogStr = await fs.promises.readFile(slugPath, 'utf-8');
             const { data } = matter(blogStr);
-            res.push(data as BlogMetadata);
-
+            
+            const slug = parseInt(file.replace('.md', ''), 10);
+            
+            res.push({
+                ...data,
+                slug
+            } as BlogMetadata);
         }
     } catch ( e : any ){
         console.error("Error reading blog dir: ", e as Error);
     }
 
-    return res;
+    return res.reverse();
 }
 
 export async function getBlogbySlug(slug: number): Promise<BlogPost> {
